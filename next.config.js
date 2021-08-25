c809880931e0bedceb7f522281b401dd32fa2f50
stevending1st <1139274654@qq.com>
2021-08-25 20:00:29 +0800

    index page and fix the bug

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+const withTypescript = require('@zeit/next-typescript')
+const withCSS = require('@zeit/next-css')
+
 module.exports = {
   reactStrictMode: true,
 }
