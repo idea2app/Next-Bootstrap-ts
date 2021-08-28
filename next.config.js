135721a7300a7698e56fc6df0580069a194fad61
Steven Ding <stevending1st@163.com>
2021-08-28 10:41:11 -0500

    vercel 部署 (#2)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const withTypescript = require('@zeit/next-typescript')
-const withCSS = require('@zeit/next-css')
 
 module.exports = {
   reactStrictMode: true,
