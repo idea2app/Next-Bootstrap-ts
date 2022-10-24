7542914ab1a9656dc1750e291116745520c52d94
Leo <leo@wangrunlin.com>
2022-10-24 10:00:07 +0800

    Revert "style(pages): fix eslint next no-sync-scripts warning"
    This reverts commit 12e541266feca7991f13521df457e35ac15319ec.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,10 +7,7 @@ export default function Document() {
         <link rel="icon" href="/favicon.ico" />
 
         <link rel="manifest" href="/manifest.json" />
-        <script
-          src="https://polyfill.web-cell.dev/feature/PWAManifest.js"
-          defer
-        ></script>
+        <script src="https://polyfill.web-cell.dev/feature/PWAManifest.js"></script>
 
         <link
           rel="stylesheet"
