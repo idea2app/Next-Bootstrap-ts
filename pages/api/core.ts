e17859e7589d98384a6a1c352fe8ab98b16d4caf
TechQuery <shiy2008@gmail.com>
2022-12-05 04:38:16 +0800

    [remove] Request & Cookie utility functions

--- pages/api/core.ts
+++ pages/api/core.ts
@@ -1,5 +1,4 @@
-import { ServerResponse } from 'http';
-import { HTTPError, Request, request as call } from 'koajax';
+import { HTTPError } from 'koajax';
 import { parseLanguageHeader } from 'mobx-i18n';
 import {
   GetServerSideProps,
@@ -9,67 +8,9 @@ import {
   NextApiRequest,
   NextApiResponse,
 } from 'next';
-import { setCookie } from 'nookies';
 import { ParsedUrlQuery } from 'querystring';
 
 import { i18n } from '../../models/Translation';
-
-const BackHost = process.env.NEXT_PUBLIC_API_HOST;
-const Host =
-  typeof window !== 'undefined'
-    ? new URL('/api/', location.origin) + ''
-    : BackHost;
-
-export async function request<T = void>(
-  path: string,
-  method?: Request['method'],
-  body?: any,
-  context?: Partial<GetServerSidePropsContext>,
-  headers: Record<string, any> = {},
-) {
-  const token = context?.req && readCookie(context.req, 'token');
-
-  if (token) headers.Authorization = `Bearer ${token}`;
-
-  try {
-    body = JSON.stringify(body);
-    headers['Content-Type'] = 'application/json';
-  } catch {}
-
-  const { response } = call<T>({
-    path: new URL(path, Host) + '',
-    method,
-    body,
-    headers,
-    responseType: 'json',
-  });
-  const { body: data } = await response;
-
-  return data!;
-}
-
-export async function requestClient<T = void>(
-  path: string,
-  method?: Request['method'],
-  body?: any,
-  headers: Record<string, any> = {},
-) {
-  try {
-    const token = '';
-
-    return request<T>(
-      new URL(path, BackHost) + '',
-      method,
-      body,
-      {},
-      { Authorization: `Bearer ${token}`, ...headers },
-    );
-  } catch (error) {
-    if (error instanceof HTTPError) location.href = `/${error.status}`;
-
-    throw error;
-  }
-}
 
 export type NextAPI = (
   req: NextApiRequest,
@@ -157,23 +98,4 @@ export function withTranslation<
       RouteProps<R> & InferGetServerSidePropsType<O>
     >;
   };
-}
-const Env = process.env.NODE_ENV;
-
-export function writeCookie(
-  res: ServerResponse,
-  key: string,
-  value: string,
-  expiredAt: string,
-) {
-  setCookie({ res }, key, value, {
-    httpOnly: true,
-    secure: Env !== 'development',
-    maxAge: +new Date(expiredAt) - Date.now(),
-    path: '/',
-  });
-}
-
-export function readCookie(req: GetServerSidePropsContext['req'], key: string) {
-  return req.cookies[key];
 }
