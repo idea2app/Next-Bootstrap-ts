c686118386166312e3573e73cd56f115cb80405a
TechQuery <shiy2008@gmail.com>
2023-08-24 03:54:49 +0800

    [optimize] upgrade MobX & other Upstream packages

--- components/Git/Logo.tsx
+++ components/Git/Logo.tsx
@@ -1,4 +1,4 @@
-import { observable } from 'mobx';
+import { makeObservable, observable } from 'mobx';
 import { observer } from 'mobx-react';
 import { PureComponent } from 'react';
 import { Image } from 'react-bootstrap';
@@ -9,6 +9,11 @@ export interface GitLogoProps {
 
 @observer
 export class GitLogo extends PureComponent<GitLogoProps> {
+  constructor(props: GitLogoProps) {
+    super(props);
+    makeObservable?.(this);
+  }
+
   @observable
   path = '';
 
