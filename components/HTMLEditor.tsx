b1c380d8fced09239a41289f7456909ad91eff52
South Drifted <shiy2008@gmail.com>
2024-07-17 13:54:44 +0000

    [add] Image Uploader  in edtior examples based on Fake API [remove] deprecated Next.js export static command [optimize] update Upstream packages

--- components/HTMLEditor.tsx
+++ components/HTMLEditor.tsx
@@ -1,19 +1,24 @@
 import {
   AudioTool,
+  CopyMarkdownTool,
   Editor,
   EditorProps,
   IFrameTool,
+  ImageTool,
   OriginalTools,
   VideoTool,
 } from 'react-bootstrap-editor';
 import { Constructor } from 'web-utility';
+
+import { upload } from '../models/Base';
 
 const ExcludeTools = [IFrameTool, AudioTool, VideoTool];
 
 const CustomTools = OriginalTools.filter(
   Tool => !ExcludeTools.includes(Tool as Constructor<IFrameTool>),
 );
+ImageTool.prototype.save = upload;
 
 export default function HTMLEditor(props: EditorProps) {
-  return <Editor tools={CustomTools} {...props} />;
+  return <Editor tools={[...CustomTools, CopyMarkdownTool]} {...props} />;
 }
--- models/Base.ts
+++ models/Base.ts
@@ -27,3 +27,18 @@ githubClient.use(({ request }, next) => {
 });
 
 export const repositoryStore = new RepositoryModel('idea2app');
+
+type UploadedFile = Record<'originalname' | 'filename' | 'location', string>;
+/**
+ * @see {@link https://fakeapi.platzi.com/en/rest/files/}
+ */
+export async function upload(file: Blob) {
+  const form = new FormData();
+  form.append('file', file);
+
+  const { body } = await ownClient.post<UploadedFile>(
+    'https://api.escuelajs.co/api/v1/files/upload',
+    form,
+  );
+  return body!.location;
+}
